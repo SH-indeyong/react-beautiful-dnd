ae991695d7cede00cfdbd5fd02b4e47171037245
SH-indeyong <ohsh1115@gmail.com>
2023-05-07 15:40:50 +0900

    feat: drag-drop

--- react-beautiful-dnd/src/App.js
+++ react-beautiful-dnd/src/App.js
@@ -1,9 +1,49 @@
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: "1", text: "text1" },
+    { id: "2", text: "text2" },
+    { id: "3", text: "text3" },
+    { id: "4", text: "text4" },
+    { id: "5", text: "text5" },
+  ]);
+
+  const handleOnDragEnd = (result) => {
+    if (!result.destination) return;
+    const itemsCopy = Array.from(items);
+    const [reorderedItem] = itemsCopy.splice(result.source.index, 1);
+    itemsCopy.splice(result.destination.index, 0, reorderedItem);
+    setItems(itemsCopy);
+  };
+
   return (
     <div className="App">
-      <p>hello</p>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="items">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef} className="column">
+              {items.map(({ id, text }, index) => (
+                <Draggable key={id} draggableId={id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className="block"
+                    >
+                      <p>{text}</p>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
